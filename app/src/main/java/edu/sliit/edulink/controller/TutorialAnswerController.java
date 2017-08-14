c914dec00b2871a4c24bee8d27995c478b1bb2e4
kygan <koumudiganepola.15@cse.mrt.ac.lk>
2017-08-14 10:50:12 +0530

    kou com tute answers

--- app/src/main/java/edu/sliit/edulink/controller/TutorialAnswerController.java
+++ app/src/main/java/edu/sliit/edulink/controller/TutorialAnswerController.java
@@ -13,7 +13,23 @@ public class TutorialAnswerController {
                         case 1:
                             return null;
                         case 2:
-                            return null;
+                            return new String[][]{
+                                    {
+                                            "Hello","Good Bye","Happy Birthday","Good Morning"
+                                    },
+                                    {
+                                            "Happy New Year","Merry Christmas","Good Night","Good Bye"
+                                    },
+                                    {
+                                            "Merry Christmas","Good Afternoon","Good Bye","Hello"
+                                    },
+                                    {
+                                            "Hello","Good Evening","Good Luck","Happy Birthday"
+                                    },
+                                    {
+                                            "Good Bye","Merry Christmas","Good Night","Good Afternoon"
+                                    }
+                            };
                         case 3:
                             return null;
                         default:
@@ -22,11 +38,62 @@ public class TutorialAnswerController {
                 } else if (subject.equals("MATH")) {
                     switch (tutorial) {
                         case 1:
-                            return null;
+                            return new String[][]{
+                                    {
+                                            "8","2","5","1"
+                                    },
+                                    {
+                                            "4","0","9","3"
+                                    },
+                                    {
+                                            "1","9","6","4"
+                                    },
+                                    {
+                                            "2","7","5","8"
+                                    },
+                                    {
+                                            "3","6","1","5"
+                                    }
+                            };
                         case 2:
-                            return null;
+                            return new String[][]{
+                                    {
+                                            "4","6","9","3"
+                                    },
+                                    {
+                                            "4","6","2","7"
+                                    },
+                                    {
+                                            "3","1","9","4"
+                                    },
+                                    {
+                                            "7","8","2","5"
+                                    },
+                                    {
+                                            "2","8","5","6"
+                                    },
+                                    {
+                                            "9","7","4","8"
+                                    }
+                            };
                         case 3:
-                            return null;
+                            return new String[][]{
+                                    {
+                                            "8","1","7","2"
+                                    },
+                                    {
+                                            "5","7","9","2"
+                                    },
+                                    {
+                                            "0","5","8","3"
+                                    },
+                                    {
+                                            "8","4","1","5"
+                                    },
+                                    {
+                                            "3","0","9","6"
+                                    }
+                            };
                         default:
                             return null;
                     }
